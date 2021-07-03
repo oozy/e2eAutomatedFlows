7db0a39ab8e45f98216936f9c979dfdb830b7115
Oozy Bitew <oozyb@wix.com>
2021-07-04 01:31:51 +0300

    refactore testing code , stuck on select option DDL

--- actionUtils/utils.js
+++ actionUtils/utils.js
@@ -17,10 +17,11 @@ const dataHookBuilder = (dataHook) => {
 
 const getElementByDataHook = async (page, dataHook) => {
   try {
-    await page.waitForSelector(dataHookBuilder(dataHook));
+    await page.waitForSelector(dataHookBuilder(dataHook), 3000);
     const selector = `${dataHookBuilder(dataHook)}`;
     await page.waitForSelector(selector);
     const element = await page.$(selector);
+    console.log('getElementByDataHook:/*/*/* Pass');
     return element;
   } catch (error) {
     console.log('typeOnInputError', error);
@@ -30,20 +31,25 @@ const getElementByDataHook = async (page, dataHook) => {
 export const generateTestAction = async (data) => {
   const { element, action, dataHook, value, page } = data;
   const testAction = element + '_' + action;
+  console.log('\n\n\n\n\n\n');
+  console.log('testAction', testAction);
   switch (testAction.trim()) {
-    case 'input_type':
-      return await typeOnInput(dataHook, value, page);
-    case 'input_delete':
-      return await deleteInputVal(dataHook, value, page);
-    case 'div_exists':
-      return await getElementByDataHook(page, dataHook);
+    case 'input_change':
+      return await OnChangeInput(dataHook, value, page);
+    case 'svg_click':
+    case 'input_click':
+      return await clickOnElement(dataHook, page);
+    case 'select_click':
+      return await clickOnElementWithValue(dataHook, page, value);
+    // case 'div_exists':
+    //   return await getElementByDataHook(page, dataHook);
     default:
       console.log('whattt???!?!?!?!?');
       break;
   }
 };
 
-export const typeOnInput = async (dataHook, value, page) => {
+export const OnChangeInput = async (dataHook, value, page) => {
   try {
     const input = await getElementByDataHook(page, dataHook);
     if (input) {
@@ -77,21 +83,35 @@ export const deleteInputVal = async (dataHook, value, page) => {
     console.log('typeOnInputError', error);
   }
 };
+export const clickOnElement = async (dataHook, page) => {
+  try {
+    const element = await getElementByDataHook(page, dataHook);
+    if (element) {
+      // await element.click();
+      await element.evaluate((e) => e.click());
+      return element;
+    }
+    return;
+  } catch (error) {
+    console.log('clickOnElement', error);
+  }
+};
 
-// export const showFragment = async (dataHook, value, page) => {
-//   console.log(123456, dataHook);
-//   try {
-//     const fragment = await getElementByDataHook(page, dataHook);
-//     console.log(`img`, fragment);
-//     console.log(`test for deleteInputVal passed `);
-//     return;
-//     // if (input) {
-//     //   await input.focus();
-//     //   await input.press('Backspace');
-//     // }
-//     // const newInput = await page.evaluate((el) => el.value, input);
-//     // assert.strictEqual(value, newInput);
-//   } catch (error) {
-//     console.log('typeOnInputError', error);
-//   }
-// };
+export const clickOnElementWithValue = async (dataHook, page, value) => {
+  try {
+    const element = await clickOnElement(dataHook, page);
+    // const option = await element.$$eval("", node =>
+    // node.value);
+
+    console.log('option', element);
+    // const option = await page.$eval(
+    //   '#tst_quantity_dropdown',
+    //   (node) => node.value,
+    // );
+    const newElementVal = await page.evaluate((el) => el.value, element);
+    assert.strictEqual(value, newElementVal);
+    console.log(`test for deleteInputVal passed `);
+  } catch (error) {
+    console.log('clickOnElementWithValue', error);
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import puppeteer from 'puppeteer';
 import fetch from 'node-fetch';
-import {
-  typeOnInput,
-  generateTestStep,
-  generateTestAction,
-} from './actionUtils/utils.js';
+import { generateTestAction } from './actionUtils/utils.js';
 
 const getTestJson = async () => {
   const data = await fetch(
@@ -13,27 +9,110 @@ const getTestJson = async () => {
     .then((response) => response.json())
     .then((data) => data);
   // return data;
+  console.log(12313213213213213132132132);
   return {
     pageUrl: 'http://localhost:3000/search',
     tests: [
       {
         element: 'input',
-        action: 'type',
+        action: 'click',
         dataHook: 'search-input',
-        value: 'oozy',
+        value: '',
+        time: 1625346434955,
+        pageUrl: 'http://localhost:3001/search',
       },
       {
         element: 'input',
-        action: 'delete',
+        action: 'click',
         dataHook: 'search-input',
         value: '',
+        time: 1625346434955,
+        pageUrl: 'http://localhost:3001/search',
       },
       {
-        element: 'div',
-        action: 'exists',
-        dataHook: 'all-books',
-        value: 'true',
+        element: 'input',
+        action: 'change',
+        dataHook: 'search-input',
+        value: 'gogo',
+        time: 1625346437159,
+        pageUrl: 'http://localhost:3001/search',
       },
+      {
+        element: 'svg',
+        action: 'click',
+        dataHook: 'imageIcon-book-0',
+        time: 1625346443191,
+        pageUrl: 'http://localhost:3001/search',
+      },
+      {
+        element: 'svg',
+        action: 'click',
+        dataHook: 'imageIcon-book-0',
+        time: 1625346443191,
+        pageUrl: 'http://localhost:3001/search',
+      },
+      {
+        element: 'select',
+        action: 'click',
+        dataHook: 'select-book-0',
+        value: '123',
+        time: 1625346443859,
+        pageUrl: 'http://localhost:3001/search',
+      },
+      // {
+      //   element: 'select',
+      //   action: 'click',
+      //   dataHook: 'select-book-0',
+      //   value: '123',
+      //   time: 1625346443859,
+      //   pageUrl: 'http://localhost:3001/search',
+      // },
+      // {
+      //   element: 'select',
+      //   action: 'change',
+      //   dataHook: 'select-book-0',
+      //   value: '321',
+      //   time: 1625346445093,
+      //   pageUrl: 'http://localhost:3001/search',
+      // },
+      // {
+      //   element: 'svg',
+      //   action: 'click',
+      //   dataHook: 'imageIcon-book-1',
+      //   time: 1625346446018,
+      //   pageUrl: 'http://localhost:3001/search',
+      // },
+      // {
+      //   element: 'svg',
+      //   action: 'click',
+      //   dataHook: 'imageIcon-book-1',
+      //   time: 1625346446018,
+      //   pageUrl: 'http://localhost:3001/search',
+      // },
+      // {
+      //   element: 'select',
+      //   action: 'click',
+      //   dataHook: 'select-book-1',
+      //   value: '123',
+      //   time: 1625346446549,
+      //   pageUrl: 'http://localhost:3001/search',
+      // },
+      // {
+      //   element: 'select',
+      //   action: 'click',
+      //   dataHook: 'select-book-1',
+      //   value: '123',
+      //   time: 1625346446549,
+      //   pageUrl: 'http://localhost:3001/search',
+      // },
+      // {
+      //   element: 'select',
+      //   action: 'change',
+      //   dataHook: 'select-book-1',
+      //   value: '321',
+      //   time: 1625346447769,
+      //   pageUrl: 'http://localhost:3001/search',
+      // },
     ],
   };
 };
@@ -41,8 +120,8 @@ const getTestJson = async () => {
 (async function() {
   const { pageUrl = '', tests = [] } = await getTestJson();
   const browser = await puppeteer.launch({
-    headless: true,
-    // slowMo: 500,
+    headless: false,
+    slowMo: 200,
     args: [
       '--no-sandbox', // I needed these args for it to run on my machine, you probably don't need them.
       '--disable-setuid-sandbox',
@@ -60,9 +139,9 @@ const getTestJson = async () => {
 })();
 
 const forLoop = async (tests, page, pageUrl) => {
+  await page.goto(pageUrl, { waitUntil: 'networkidle0' });
   for (let index = 0; index < tests.length; index++) {
     const test = tests[index];
-    await page.goto(pageUrl, { waitUntil: 'networkidle0' });
     const data = { page, ...test };
     await generateTestAction({ ...data });
   }
