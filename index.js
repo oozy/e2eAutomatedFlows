b26253aac62f52c9595a67b550566deb3bc3865d
oozyb <oozyb@wix.com>
2021-07-30 22:26:48 +0300

    increse time outs

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import express from 'express';
 import { runTest } from './testRuner';
-import bodyParser from 'body-parser';
+var timeout = require('connect-timeout');
+
+// import bodyParser from 'body-parser';
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(haltOnTimedout);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.end('Hello World!');
 });
 
-app.post('/task', async (req, res) => {
-  req.setTimeout(500000);
+function haltOnTimedout(req, res, next) {
+	if (!req.timedout) next();
+}
+app.post('/task', timeout(500000), async (req, res) => {
+	req.setTimeout(500000);
 	global.globalString = 100;
 	const { taskId, pageUrl } = req.body;
 	await runTest(taskId, pageUrl);
+	if (req.timedout) return;
 	res.send(` globalString :  ${globalString}`);
 });
 
